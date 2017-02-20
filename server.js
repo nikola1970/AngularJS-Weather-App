3665b90c32230d959ca809942e4ae61aef427ca9
Nikola Simovic <simovic.nikola90@gmail.com>
2017-02-20 21:12:10 +0100

    https fix

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ var express = require("express");
 
 var app = express();
 
+app.use(function (req, res, next) {
+	if (req.headers['x-forwarded-proto'] === 'https') {
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+});
+
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
 	console.log('Express is running!');
 });
